1e53605c66b2838382b9260117a380b57acf6a80
OmJoshii <omjoshii192@gmail.com>
2024-07-07 19:07:33 +0545

    MyInfo

---
+++ MyInfo/src/MyInfo/MyInfo.java
@@ -0,0 +1,14 @@
+package MyInfo;
+public class MyInfo {
+    String name = "Om";
+    String phone = "9863576413";
+    String address ="Satdobato";
+    public static void main(String[] args){
+        MyInfo studentObject = new MyInfo();
+        studentObject.name="Sanam";
+        studentObject.phone="987654321";
+        studentObject.address="Lalitpur";
+        System.out.println(studentObject.name+" "+studentObject.phone+" "+studentObject.address+"\n");
+    
+}
+}
